c8a7fa65cc958481038b1e4fb0dd0904283c3ab9
murilo_kronbauer <murilokronbauer@gmail.com>
2019-05-31 16:24:43 -0300

    Adicionado Static (FUNCIONANDO), Html com forms.

--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+
+def formulario(request):
+    return render(request, 'app/formulario.html')
