d0d4365c227a432c60d5c4ade7929e40d6565ac3
arthur <arthur.pcorrea@sempreceub.com>
2019-06-03 18:02:59 -0300

    Arrumando models

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,21 @@
 from django import forms
 from app.models import TB_Usuario
 
-AVALIACOES = [
-    ('Excelente', 10),
-    ('Bom', 8),
-    ('Médio', 5),
-    ('Ruim', 3),
-    ('Péssimo', 1)
-]
-
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = TB_Usuario
+<<<<<<< Updated upstream
         fields = '__all__' #['email', 'senha', 'avaliacao', 'notificacao', 'comentarios']
         widgets = {
             'email': forms.EmailField(attrs={'placeholder': 'exemplo@exemplo.com'}),
             'senha': forms.PasswordInput(),
             'avaliacao': forms.ChoiceField(choices=AVALIACOES)
         }
+=======
+        fields = "__all__"
+        # widgets = {
+        #      'email': forms.EmailField(attrs={'placeholder': 'exemplo@exemplo.com'}),
+        #      'senha': forms.PasswordInput(),
+        #  }
+>>>>>>> Stashed changes
--- app/models.py
+++ app/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 class TB_Usuario(models.Model):
+<<<<<<< Updated upstream
     id = models.AutoField(primary_key=True, name='id')
+=======
+    AVALIACOES = [
+        ('Excelente', 'Excelente'),
+        ('Bom', 'Bom'),
+        ('Médio', 'Médio'),
+        ('Ruim', 'Ruim'),
+        ('Péssimo', 'Péssimo'),
+    ]
+    cpf = models.CharField(primary_key=True, max_length=11, name='cpf')
+>>>>>>> Stashed changes
     email = models.EmailField(name='email')
     senha = models.CharField(max_length=20, name='senha')
     notificacao = models.BooleanField(name='notificacao')
-    avaliacao = models.CharField(max_length=5, name='avaliacao')
+    avaliacao = models.CharField(max_length=10, name='avaliacao', choices=AVALIACOES)
     comentarios = models.CharField(max_length=500, name='comentarios')
     class Meta:
         db_table = "TB_Usuarios"
