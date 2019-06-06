953737e5b7438519f4aa8e39085b645a787e0b45
arthur <arthur.pcorrea@sempreceub.com>
2019-06-06 18:42:41 -0300

    CRUD funcionando

--- app/views.py
+++ app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from app.forms import UsuarioForm
 from app.models import TB_Usuario
 
+
 def home(request):
     return render(request, 'app/home.html')
+
 
 def create(request):
     if request.method == "POST":
@@ -15,21 +17,32 @@ def create(request):
         form = UsuarioForm()
         return render(request, 'app/create.html', {'form': form})
 
+
 def read(request):
     dados = {}
     dados['usuarios'] = TB_Usuario.objects.all()
     return render(request, 'app/read.html', dados)
 
+
 def update(request, cpf):
     dados = {}
     usuario = TB_Usuario.objects.get(cpf=cpf)
-    form = UsuarioForm(request.POST or None, instance=usuario)
     dados['usuario'] = usuario
-    if form.is_valid():
-        form.save()
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, instance=usuario)
         dados['form'] = form
-        return redirect('/read/')
+        print(form.is_valid())
+        if form.is_valid():
+            print('valido')
+            form.save()
+            return redirect('/read')
+    else:
+        form = UsuarioForm()
+        dados['form'] = form
     return render(request, 'app/update.html', dados)
 
-def delete(request, pk):
-    return render(request, 'app/home.html')
+
+def delete(request, cpf):
+    usuario = TB_Usuario.objects.get(cpf=cpf)
+    usuario.delete()
+    return redirect('/read')
